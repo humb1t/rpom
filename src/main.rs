8c259aebe381ba356fa42316081fe24d3430720d
Nikita Puzankov <humb1t@yandex.ru>
2018-08-15 14:57:40 +0400

    Order as a separate module

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,10 @@ extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
 
-use diesel::prelude::*;
-use diesel::result::QueryResult;
-use domain::Order;
 use dotenv::dotenv;
-use rocket_contrib::Json;
 use std::env;
 
+pub mod order;
 pub mod schema;
 
 mod db_pool {
@@ -65,54 +62,6 @@ mod web {
     }
 }
 
-mod domain {
-    use diesel::prelude::*;
-    use schema::orders;
-
-    #[derive(Serialize, Deserialize, Queryable, Insertable, Copy, Clone)]
-    #[table_name = "orders"]
-    pub struct Order {
-        pub id: i32,
-        pub specification_id: i32,
-        pub quantity: i32,
-    }
-}
-
-#[post("/orders", data = "<order>")]
-fn order_create(order: Json<Order>, conn: db_pool::DbConn) -> Json<Order> {
-    use schema::orders::dsl::*;
-    let order = order.0;
-    let pid: i32 = order.id;
-    diesel::insert_into(orders)
-        .values(&order)
-        .execute(&*conn)
-        .expect("Error saving new order");
-    let order = orders
-        .find(pid)
-        .first::<Order>(&*conn)
-        .expect("Error loading order");
-
-    Json(order)
-}
-
-#[get("/orders/<fid>")]
-fn order_get(fid: i32, conn: db_pool::DbConn) -> Json<Order> {
-    use schema::orders::dsl::*;
-    let order = orders
-        .find(fid)
-        .first::<Order>(&*conn)
-        .expect("Error loading order");
-
-    Json(order)
-}
-
-#[get("/orders")]
-fn order_get_all(conn: db_pool::DbConn) -> QueryResult<Json<Vec<Order>>> {
-    use schema::orders::dsl::*;
-    orders.load::<Order>(&*conn)
-        .map(|ordrs| Json(ordrs))
-}
-
 fn main() {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL")
@@ -120,9 +69,9 @@ fn main() {
     rocket::ignite().manage(db_pool::init_pool(&database_url)).mount(
         "/",
         routes![
-           order_create,
-           order_get,
-           order_get_all
+           order::create,
+           order::get,
+           order::get_all
          ],
     ).launch();
 }
---
+++ src/order.rs
@@ -0,0 +1,48 @@
+use db_pool;
+use diesel;
+use diesel::prelude::*;
+use rocket_contrib::Json;
+use schema::orders;
+
+#[derive(Serialize, Deserialize, Queryable, Insertable, Copy, Clone)]
+#[table_name = "orders"]
+pub struct Order {
+    pub id: i32,
+    pub specification_id: i32,
+    pub quantity: i32,
+}
+
+#[post("/orders", data = "<order>")]
+fn create(order: Json<Order>, conn: db_pool::DbConn) -> Json<Order> {
+    use schema::orders::dsl::*;
+    let order: Order = order.0;
+    let pid: i32 = order.id;
+    diesel::insert_into(orders)
+        .values(&order)
+        .execute(&*conn)
+        .expect("Error saving new order");
+    let order = orders
+        .find(pid)
+        .first::<Order>(&*conn)
+        .expect("Error loading order");
+
+    Json(order)
+}
+
+#[get("/orders/<fid>")]
+fn get(fid: i32, conn: db_pool::DbConn) -> Json<Order> {
+    use schema::orders::dsl::*;
+    let order = orders
+        .find(fid)
+        .first::<Order>(&*conn)
+        .expect("Error loading order");
+
+    Json(order)
+}
+
+#[get("/orders")]
+fn get_all(conn: db_pool::DbConn) -> QueryResult<Json<Vec<Order>>> {
+    use schema::orders::dsl::*;
+    orders.load::<Order>(&*conn)
+        .map(|ordrs| Json(ordrs))
+}
